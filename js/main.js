779a2561c13f4d441475f03442e8d54d5dfdc910
Luis Leao <lleao@lleao-macbookpro.roam.corp.google.com>
2015-09-03 15:37:11 +0800

    populating firebase scripts

--- js/main.js
+++ js/main.js
@@ -14,7 +14,21 @@ myFirebaseRef.set({
   }
 });
 
-$.getJSON("database.json", function(data){
-  myFirebaseRef.set(data);
+var myFirebaseRef = new Firebase("https://polipoll.firebaseio.com/");
+
+var projects = {};
+
+$.getJSON("database.json", function(data_projects){
+    projects = data_projects;
+    $.getJSON("database.json", function(data){
+    console.log("saving data...");
+    myFirebaseRef.set({
+    "politicians": data,
+    "projects": data_projects
+  });
+    console.log("data saved");
+  });
 });
+
+
 */
