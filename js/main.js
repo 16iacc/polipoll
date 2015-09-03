1c48876078ac25fdb06d7748ad05fe5b181e4732
Luis Leao <lleao@lleao-macbookpro.roam.corp.google.com>
2015-09-03 15:29:37 +0800

    database.json and jquery

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var myFirebaseRef = new Firebase("https://polipoll.firebaseio.com/");
+
+
+
+/*
+
+myFirebaseRef.set({
+  title: "Hello World 2015!",
+  author: "Firebase",
+  location: {
+    city: "San Francisco",
+    state: "California",
+    zip: 94103
+  }
+});
+
+$.getJSON("database.json", function(data){
+  myFirebaseRef.set(data);
+});
+*/
